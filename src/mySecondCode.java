be85a37f39fdf7298214b83af548f51c1c592771
Yusuf Bayram <yusufbayram19891989@gmail.com>
2022-03-27 17:23:05 +0100

    hello

--- src/mySecondCode.java
+++ src/mySecondCode.java
@@ -1,5 +1,7 @@
 public class mySecondCode {
     public static void main(String[] args) {
         System.out.println("Hello world");
+
+        System.out.println("this is another one ");
     }
 }

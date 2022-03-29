5571c41512b9b903d1a116e0a209139944b193a8
Yusuf Bayram <yusufbayram19891989@gmail.com>
2022-03-29 02:36:00 +0100

    Lambda

---
+++ src/day01notes/Lambda.java
@@ -0,0 +1,9 @@
+package day01notes;
+
+public class Lambda {
+    // Lambda is a functional programming, we have had Lambda in Java with version 8
+    //        Functional programming focuses on "What to do", with structured programming we focus on "How to do"
+    //        Functional programming is used just with Collections and Arrays
+    //        We cannot use Maps directly in functional programming, but we can convert Maps to Sets and use them
+    //        we can use entrySet()
+}

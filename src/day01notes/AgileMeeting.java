4db89822ddcf69d148c126b1b5958c22d4b1a06f
Yusuf Bayram <yusufbayram19891989@gmail.com>
2022-03-29 02:32:25 +0100

    Agile Meetings

---
+++ src/day01notes/AgileMeeting.java
@@ -0,0 +1,35 @@
+package day01notes;
+
+public class AgileMeeting {
+    /*Agile Meetings*/
+    //Sprint planning meetings
+    /* a spring planning meeting sets up your entire team for success throughout the sprint.
+    They take place at the start of each sprint and should last approximately
+     an hour per week-length of the sprint. */
+    //The goal of the sprint planning session is for the scrum team to meet and have a conversation
+    // regarding what work they want to accomplish in the next sprint, and prioritize each task and
+    // assign it to each sprint member.
+
+    /*2 Scrum meetings*/
+    //A scrum meeting, otherwise referred to as a daily stand-up,
+    // is a quick check-in with your team. They occur once per day for every day of the sprint,
+    // are usually no more than 15 minutes in length, and typically take place in the morning.
+    /*During these stand-ups, each member of the development team will quickly share with other members exactly
+     what they’re working on, how their progress is going, and if they’re experiencing any blockers.*/
+
+    /*3 Backlog grooming meetings*/
+    //A backlog grooming meeting, sometimes referred to as a backlog refinement meeting or
+    // a grooming scrum, is used to review items on the backlog.
+    // In this case, a backlog is a list of any upcoming items and things that need to be done by your team.
+    // The goal of these meetings is to keep the backlog as accurate and up to date as possible so
+    // it can be pulled and referencing in upcoming sprints.
+    /*Who attends backlog grooming meetings?
+    Backlog grooming meetings can occur on a regular basis but typically take place towards the end of a sprint.
+    They’re usually between 30 minutes and one hour in length and
+    in attendance are the scrum master, product owner, and the development team. */
+
+    /*4 Sprint retrospective meeting */
+    //During a sprint retrospective meeting, the development team, scrum master,
+    // and product owner will all discuss their work together on the specific project or agenda.
+
+}

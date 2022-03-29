21748b59f5fadb8162b241b503414bfb186a2681
Yusuf Bayram <yusufbayram19891989@gmail.com>
2022-03-29 03:13:26 +0100

    Repository comments

---
+++ src/day01notes/ExistingRepository.java
@@ -0,0 +1,7 @@
+package day01notes;
+
+public class ExistingRepository {
+    //git remote add origin https://github.com/yusufby/yusufby.git
+    //git branch -M main
+    //git push -u origin main
+}
---
+++ src/day01notes/NewRepository.java
@@ -0,0 +1,12 @@
+package day01notes;
+
+public class NewRepository {
+    /*echo "# yusufby" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/yusufby/yusufby.git
+git push -u origin main
+*/
+}

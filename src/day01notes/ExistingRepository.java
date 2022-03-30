e2e484fe7db2d81e88f471c4544826981ce4e408
Yusuf Bayram <yusufbayram19891989@gmail.com>
2022-03-30 11:51:13 +0100

    new addition

--- src/day01notes/ExistingRepository.java
+++ src/day01notes/ExistingRepository.java
@@ -3,5 +3,5 @@ package day01notes;
 public class ExistingRepository {
     //git remote add origin https://github.com/yusufby/yusufby.git
     //git branch -M main
-    //git push -u origin main
+    //git push -u origin main..
 }

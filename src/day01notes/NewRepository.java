7c137db159a0e59698023fbd983292ac3c8555e0
Yusuf Bayram <yusufbayram19891989@gmail.com>
2022-04-01 23:14:21 +0100

    neww

--- src/day01notes/NewRepository.java
+++ src/day01notes/NewRepository.java
@@ -7,6 +7,6 @@ git add README.md
 git commit -m "first commit"
 git branch -M main
 git remote add origin https://github.com/yusufby/yusufby.git
-git push -u origin main.//
+git push -u origin main.///
 */
 }

8f48903ef7cd7a9be9d561897d963b8354e62ca3
Yusuf Bayram <yusufbayram19891989@gmail.com>
2022-04-02 03:19:43 +0100

    java new questions

---
+++ src/javaquestions/Q03.java
@@ -0,0 +1,15 @@
+package javaquestions;
+
+public class Q03 {
+    public static void main(String[] args) {
+        /*Write a code that returns the duplicate chars in a String array.(interview Question)
+
+Input :
+
+String str=“Javaisalsoeasy”
+
+Output: [a, s]*/
+
+
+    }
+}

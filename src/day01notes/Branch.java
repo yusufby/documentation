b2388468a7cc70e13e84048a5c83f35e23227be4
Yusuf Bayram <yusufbayram19891989@gmail.com>
2022-04-03 05:21:24 +0100

    Branch notes

---
+++ src/day01notes/Branch.java
@@ -0,0 +1,16 @@
+package day01notes;
+
+public class Branch {
+    /*git branch OR git branch -a -> Shows all branches. Default branch is the master branch
+     * git branch feature        -> creates new branch named feature.
+     * This is the copy based on current branch.
+     * git checkout feature      -> switch to feature branch
+     * git branch -d feature       -> delete feature branch
+     * git checkout master        -> switch to master branch
+     * git merge feature        -> If you run this when you are on master branch,
+     *  then feature branch codes merges into master branch
+     *git merge master            -> If you run this when you are on feature branch,
+     * then master branch codes merges into feature branch
+     git push --set-upstream origin feature -> use when you push for the FIRST TIME.
+     After the first time, git push is enough */
+}

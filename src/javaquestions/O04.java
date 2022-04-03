5a32a6a243e9487642431657f721d21c865c01df
Yusuf Bayram <yusufbayram19891989@gmail.com>
2022-04-03 01:10:38 +0100

    java question

---
+++ src/javaquestions/O04.java
@@ -0,0 +1,10 @@
+package javaquestions;
+
+public class O04 {
+    public static void main(String[] args) {
+        /*Write a return method that accepts an integer as input and calculates
+        factorial and prints like output.
+Input : 6
+Output: 6!=65432*1=720*/
+    }
+}
